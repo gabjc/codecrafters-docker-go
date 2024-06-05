1e0140951f720d3e0465288aec564d5328c63c3f
gabjc <gabtez23@gmail.com>
2024-06-05 13:33:17 -0400

    pass stage

--- app/main.go
+++ app/main.go
@@ -17,11 +17,15 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 	cmd := exec.Command(command, args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(string(output))
+	fmt.Print(string(output))
 }
