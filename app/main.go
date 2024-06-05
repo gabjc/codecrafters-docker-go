23d8ecfebe058a2dc4c0328c01425787222a99cf
gabjc <gabtez23@gmail.com>
2024-06-04 21:32:45 -0400

    pass stage

--- app/main.go
+++ app/main.go
@@ -15,7 +15,7 @@ func main() {
 	// Uncomment this block to pass the first stage!
 
 	command := os.Args[3]
-	args := os.Args[3:len(os.Args)]
+	args := os.Args[4:len(os.Args)]
 	cmd := exec.Command(command, args...)
 	output, err := cmd.Output()
 	if err != nil {
